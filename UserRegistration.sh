96fe95fa9bc7602cfe79d8028e82d25b63657dc6
ajsushant1 <sushantajgaonkar1766@gmail.com>
2020-02-29 12:57:24 +0530

    ADDED: Regex pattern to validate first name of user

--- UserRegistration.sh
+++ UserRegistration.sh
@@ -1,3 +1,20 @@
 #!/bin/bash -x
 
 echo "/******************************************* WELCOME TO USER REGISTRATION *******************************************/"
+
+# variables
+firstName=""
+pattern=""
+
+# READING FIRST NAME FROM USER
+read -p "Enter Your First Name :" firstName
+pattern="^[A-Z]{1}[a-z]{2,}$"
+
+# VALIDATIING FIRST NAME OF USER
+if [[ $firstName =~ $pattern ]]
+then
+	echo "Valid name"
+else
+	echo "Invalid name"
+fi
+
