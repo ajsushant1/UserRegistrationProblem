ad006e6e59cc25b0da53c353747d83e1fa586072
ajsushant1 <sushantajgaonkar1766@gmail.com>
2020-02-29 19:17:43 +0530

    ADDED: Seperate Function for firstname and lastname validation,email,mobilenumber validation

--- UserRegistration.sh
+++ UserRegistration.sh
@@ -2,20 +2,25 @@
 
 echo "/******************************************* WELCOME TO USER REGISTRATION *******************************************/"
 
+#CONSTANT
+IS_EMPTY=" "
+
 # variables
 firstName=""
 lastName=""
+email=""
+mobileNumber=""
 
 # DEFINING REGEX PATTERN
-nameRegexpattern="^[A-Z][a-z]{2,}$"
+nameRegexPattern="^[A-Z][a-zA-Z]{2,}$"
 emailRegexPattern="^[a-zA-Z0-9]+([._+-]?[a-zA-Z0-9]+)?[@][a-zA-Z]+[.][a-zA-Z]{2,3}([.][a-zA-Z]{2,3})?$"
+mobileRegexPattern="^[0-9]{2}[ ][0-9]{10}$"
 
 
 # FUNCTION TO VALIDATE INPUT PATTERN
-function validation(){
+function validateFirstLastName(){
 	local inputPattern=$1
-	local regexPattern=$2
-	if [[ $inputPattern =~ $regexPattern ]]
+	if [[ $inputPattern =~ $nameRegexPattern ]]
 	then
 		echo "Valid"
 	else
@@ -23,12 +28,34 @@ function validation(){
 	fi
 }
 
+function validateEmailId(){
+	local inputPattern=$1
+	if [[ $inputPattern =~ $emailRegexPattern ]]
+	then
+		echo "Valid"
+	else
+		echo "Invalid Email Id"
+	fi
+}
+
+function validateMobileNumber(){
+	if [[ $mobileNumber =~ $mobileRegexPattern ]]
+	then
+		echo "Valid"
+	else
+		echo "Invalid Mobile Number"
+	fi
+}
+
 # READING INPUT FROM USER
 read -p "Enter Your First Name :" firstName
-validation $firstName $nameRegexpattern
+validateFirstLastName $firstName
 
 read -p "Enter Your Last Name :" lastName
-validation $lastName $nameRegexPattern
+validateFirstLastName $lastName
 
-read -p "Enter Your emailId :" email
-validation $email $emailRegexPattern
+read -p "Enter Your EmailId :" email
+validateEmailId $email
+
+read -p "Enter Your MobileNumber :" mobileNumber
+validateMobileNumber
