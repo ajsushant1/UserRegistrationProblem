4c30e1b4704e5e2385d5c1ba5382b5ad37a013d8
ajsushant1 <sushantajgaonkar1766@gmail.com>
2020-02-29 13:19:07 +0530

    ADDED: Function to validate first name and last name

--- UserRegistration.sh
+++ UserRegistration.sh
@@ -4,17 +4,23 @@ echo "/******************************************* WELCOME TO USER REGISTRATION 
 
 # variables
 firstName=""
-pattern=""
+lastName=""
 
 # READING FIRST NAME FROM USER
 read -p "Enter Your First Name :" firstName
-pattern="^[A-Z]{1}[a-z]{2,}$"
+read -p "Enter Your First Name :" lastName
 
-# VALIDATIING FIRST NAME OF USER
-if [[ $firstName =~ $pattern ]]
-then
-	echo "Valid name"
-else
-	echo "Invalid name"
-fi
+# FUNCTION TO VALIDATE FIRST NAME AND LAST NAME OF USER
+function validateName(){
+	local inputName=$1
+	local pattern="^[A-Z]{1}[a-z]{2,}$"
+	if [[ $inputName =~ $pattern ]]
+	then
+		echo "Valid"
+	else
+		echo "Invalid"
+	fi
+}
 
+validateName $firstName
+validateName $lastName
