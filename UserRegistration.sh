1d8b7ee7819aede6324e6d4cdb62bb93c64df33c
ajsushant1 <sushantajgaonkar1766@gmail.com>
2020-03-02 12:11:50 +0530

    REFACTORED: validatePasword function to check password cotains at least 1 numeric number

--- UserRegistration.sh
+++ UserRegistration.sh
@@ -8,12 +8,13 @@ lastName=""
 email=""
 mobileNumber=""
 password=""
+passwordMinimumLength=8
 
 # DEFINING REGEX PATTERN
 nameRegexPattern="^[A-Z][a-zA-Z]{2,}$"
 emailRegexPattern="^[a-zA-Z0-9]+([._+-]?[a-zA-Z0-9]+)?[@][a-zA-Z]+[.][a-zA-Z]{2,3}([.][a-zA-Z]{2,3})?$"
 mobileRegexPattern="^[0-9]{2}[ ][0-9]{10}$"
-passwordRegexPattern="^[a-zA-Z]*[A-Z]+[a-zA-Z]*{8,}$"
+passwordRegexPattern="^([a-zA-Z]*[A-Z]+[a-zA-Z]*[0-9]+)+|([0-9]+[a-zA-Z]*[A-Z]+[a-zA-Z]*)+$"
 
 # FUNCTION TO VALIDATE FIRST NAME AND LAST NAME OF USER
 function validateFirstLastName(){
@@ -50,12 +51,13 @@ function validateMobileNumber(){
 # FUNCTION TO VALIDATE USER PASSWORD
 function validatePassword(){
 	local inputPattern=$1
-	echo "$password"
-	if [[ $inputPattern =~ $passwordRegexPattern ]]
+	if [[ $inputPattern =~ $passwordRegexPattern && ${#inputPattern} -ge $passwordMinimumLength ]]
 	then
 		echo "Valid"
 	else
-		echo "Invalid Password"
+		echo "Invalid Password please enter minimum 8 character password"
+		echo "Should have at least 1 upper case letter"
+		echo "Should have at least 1 numeric number"
 	fi
 }
 
